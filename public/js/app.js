5cac0e6774b1c3addc51dac9f7f56beeb887e407
Andres Garcia <andresgarcia809@hotmail.com>
2018-09-20 20:02:21 -0400

    Finish entire design in mobile

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,12 @@ var navEl = document.querySelector('.main__nav');
 var hamburgerMenuEl = document.querySelector('.hamburger__menu');
 hamburgerMenuEl.onclick = function() {
   navEl.classList.add('open')
+};
 
-  // *Closes the nav
+// *Closes the nav
   window.addEventListener('click', function(e) {
-    if(event.target === navEl) {
+    var spanXEl = document.querySelector('#x');
+    if(event.target === navEl ||event.target === spanXEl) {
       navEl.classList.remove('open')
     }
   })
-};
